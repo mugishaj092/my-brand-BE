3e41f1c2c72af425cd0a6c6e83255d2615c5f513
“mugishaj092” <mugishajoseph092@gmail.com>
2024-04-17 13:48:46 +0200

    .env

--- src/routes/useRouter.ts
+++ src/routes/useRouter.ts
@@ -1,33 +1,33 @@
-const express = require('express')
-const userController = require('./../controllers/userController')
-const authController = require('./../controllers/authController')
+const express = require('express');
+const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/signup', authController.signUp)
-router.post('/login', authController.login)
-router.post('/forgetpassword', authController.forgetPassword)
-router.patch('/resetPassword/:token', authController.resetPassword)
+router.post('/signup', authController.signUp);
+router.post('/login', authController.login);
+router.post('/forgetpassword', authController.forgetPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
 
 router
-    .route('/')
-    .get(
-        authController.protect,
-        authController.restrictTo('admin'),
-        userController.GetAllUsers
-    )
+  .route('/')
+  .get(
+    authController.protect,
+    authController.restrictTo('admin'),
+    userController.GetAllUsers,
+  );
 router
-    .route('/:id')
-    .get(
-        authController.protect,
-        authController.restrictTo('admin'),
-        userController.GetSingleUser
-    )
-    .patch(authController.protect, userController.UpdateUser)
-    .delete(
-        authController.protect,
-        authController.restrictTo('admin'),
-        userController.DeleteUser
-    )
+  .route('/:id')
+  .get(
+    authController.protect,
+    authController.restrictTo('admin'),
+    userController.GetSingleUser,
+  )
+  .patch(authController.protect, userController.UpdateUser)
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin'),
+    userController.DeleteUser,
+  );
 
-export default router
+export default router;
